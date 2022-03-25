073c2f071ba894ec115152a84ecfda3f4b1dd308
Daniel O <danielogunlana23@gmail.com>
2022-03-24 20:51:08 -0400

    removed game html and generated tiles with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,51 @@
+const gameContainer = document.querySelector(".game-container")
 const tile = document.querySelector(".tile");
 const tileRow = document.querySelector(".tile-row");
+const NUMBER_OF_GUESSES = 6;
+
+// function to generate tiles onto screen 
+const initBoard = () => {
+  let board = document.getElementById("board");
+
+  for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+    let tileRow = document.createElement("div");
+    tileRow.className = "tile-row"
+
+    for (let j = 0; j < 5; j++) {
+      let tile = document.createElement("div");
+      tile.className = "tile"
+      tileRow.appendChild(tile)
+    }
+    board.appendChild(tileRow)
+  }
+}
+initBoard()
+
+// const NUMBER_OF_GUESSES = 6;
+// let guessesRemaining = NUMBER_OF_GUESSES;
+// let currentGuess = [];
+// let nextLetter = 0;
+// let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+// console.log(rightGuessString)
+
 // detect keypress
 // set datastate of each tile to absent if the letter isnt present in the word
 // set datastate of each tile to present if the letter is present in the word
 // set datastate of each tile to correct if the letter is present and at the right position
-document.addEventListener("keydown", function (e) {
-  e.preventDefault()
-  let key = e.key
-  console.log(key)
-  // If a letter is pressed update tile
-  let alphabetCharacters = "abcdefghijklmnopqrstuvwxyz ".split("");
+// document.addEventListener("keydown", function (e) {
+//   e.preventDefault()
+//   let key = e.key
+//   console.log(key)
+//   // If a letter is pressed update tile
 
-  if (alphabetCharacters.includes(key)) {
-    console.log("no")
-    // If its backspace delete last letter of word
-  } else if (key = "Backspace") {
-    console.log("yes")
-  }
-})
 
+
+//   let alphabetCharacters = "abcdefghijklmnopqrstuvwxyz ".split("");
+
+//   if (alphabetCharacters.includes(key)) {
+//     console.log("no")
+//     // If its backspace delete last letter of word
+//   } else if (key = "Backspace") {
+//     console.log("yes")
+//   }
+// })
